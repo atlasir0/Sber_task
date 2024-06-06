4a622afb0d92bbd4d5ad6f3c6040c59f1b901b1f
Atlas Ironic <rinat.kurmakaev@outlook.com>
2024-06-06 20:53:57 +0300

    Update main application file

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"todolist/internal/api"
 	"todolist/internal/db"
-	"todolist/internal/handlers_api"
-	"todolist/internal/repositories"
-	"todolist/internal/services"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	db, err := db.InitDB()
+	_, err := db.InitDB()
 	if err != nil {
 		log.Fatalf("could not connect to the database: %v", err)
 	}
@@ -25,11 +22,6 @@ func main() {
 		log.Fatalf("could not create API instance: %v", err)
 	}
 	apiInstance.RegisterHandlers()
-
-	taskRepo := repositories.NewTaskRepository(db)
-	taskService := services.NewTaskService(taskRepo)
-	taskHandler := handlers_api.NewTaskHandler(taskService)
-	taskHandler.RegisterRoutes(router)
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", router); err != nil {
