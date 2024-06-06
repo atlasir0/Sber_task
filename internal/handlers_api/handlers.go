c0fa955d20946a83945492d782c5900e29bdf815
Atlas Ironic <rinat.kurmakaev@outlook.com>
2024-06-06 20:56:12 +0300

    Update API handlers

--- internal/handlers_api/handlers.go
+++ internal/handlers_api/handlers.go
@@ -4,14 +4,18 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+	"time"
 	"todolist/internal/models"
 	"todolist/internal/services"
 
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
 func (h *TaskHandler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/tasks/paginated", h.ListPaginatedTasks).Methods("GET")
 	router.HandleFunc("/tasks", h.CreateTask).Methods("POST")
+	router.HandleFunc("/tasks/filter", h.FilterTasksByDateAndStatus).Methods("GET")
 	router.HandleFunc("/tasks", h.ListTasks).Methods("GET")
 	router.HandleFunc("/tasks/{id}", h.GetTaskByID).Methods("GET")
 	router.HandleFunc("/tasks/{id}", h.UpdateTask).Methods("PUT")
@@ -116,4 +120,76 @@ func (h *TaskHandler) DeleteTask(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+func (h *TaskHandler) FilterTasksByDateAndStatus(w http.ResponseWriter, r *http.Request) {
+	logrus.Info("Inside FilterTasksByDateAndStatus function")
+
+	dateStr := r.URL.Query().Get("date")
+	statusStr := r.URL.Query().Get("status")
+
+	logrus.Printf("Date: %s, Status: %s\n", dateStr, statusStr)
+
+	if dateStr == "" || statusStr == "" {
+		http.Error(w, "Missing date or status parameter", http.StatusBadRequest)
+		return
+	}
+
+	date, err := time.Parse(time.RFC3339, dateStr)
+	if err != nil {
+		logrus.Errorf("Error parsing date: %v", err)
+		http.Error(w, "Invalid date format", http.StatusBadRequest)
+		return
+	}
+
+	completed, err := strconv.ParseBool(statusStr)
+	if err != nil {
+		logrus.Errorf("Error parsing status: %v", err)
+		http.Error(w, "Invalid status format", http.StatusBadRequest)
+		return
+	}
+
+	tasks, err := h.TaskService.FilterTasksByDateAndStatus(date, completed)
+	if err != nil {
+		logrus.Errorf("Error filtering tasks: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func (h *TaskHandler) ListPaginatedTasks(w http.ResponseWriter, r *http.Request) {
+	pageStr := r.URL.Query().Get("page")
+	limitStr := r.URL.Query().Get("limit")
+	statusStr := r.URL.Query().Get("status")
+
+	page, err := strconv.Atoi(pageStr)
+	if err != nil || page < 1 {
+		http.Error(w, "Invalid page parameter", http.StatusBadRequest)
+		logrus.Errorf("Invalid page parameter: %v", err)
+		return
+	}
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil || limit < 1 {
+		http.Error(w, "Invalid limit parameter", http.StatusBadRequest)
+		logrus.Errorf("Invalid limit parameter: %v", err)
+		return
+	}
+	status, err := strconv.ParseBool(statusStr)
+	if err != nil {
+		http.Error(w, "Invalid status parameter", http.StatusBadRequest)
+		logrus.Errorf("Invalid status parameter: %v", err)
+		return
+	}
+
+	tasks, err := h.TaskService.ListPaginatedTasks(page, limit, status)
+	if err != nil {
+		http.Error(w, "Failed to get tasks", http.StatusInternalServerError)
+		logrus.Errorf("Failed to get tasks: %v", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
 }
